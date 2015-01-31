d87564b94d2701db3fd043fe60bf0f9cc60531db
Deon Moolman <deon@yuppiechef.com>
2015-01-31 23:09:31 +0200

    Get the sentinel value in - only respects a single batch now though...

--- src/onyx/plugin/dynamodb.clj
+++ src/onyx/plugin/dynamodb.clj
@@ -12,9 +12,12 @@
 
 (defmethod p-ext/read-batch [:input :dynamodb-scan]
   [{:keys [onyx.core/task-map onyx.core/task :dynamodb/last-prim-kvs] :as pipeline}]
+  
   (cond
     (nil? @last-prim-kvs)
-    [:done] ;; <== What should this be?! ai.
+    (do
+      (info "Stopping")
+      {:onyx.core/batch [{:input task :message :done}]})
     :else
     (let [opts (:dynamodb/scan-options task-map)
           last @last-prim-kvs
@@ -28,11 +31,21 @@
       (info "Result: " result)
       (reset! last-prim-kvs last-kv)
       {:onyx.core/batch
-       (map (fn [r] {:input task :message r}) result)})))
+       (map (fn [r] {:input task :message r}) (concat result [:done]))})))
 
-(defmethod p-ext/compress-batch [:input :dynamodb-scan]
-  [{:keys [onyx.core/decompressed] :as pipeline}]
-  {:onyx.core/compressed decompressed})
+(defmethod p-ext/decompress-batch [:input :dynamodb-scan]
+  [{:keys [onyx.core/batch] :as event}]
+  {:onyx.core/decompressed (filter identity (map :message batch))})
+
+(defmethod p-ext/strip-sentinel [:input :dynamodb-scan]
+  [{:keys [onyx.core/decompressed]}]
+  {:onyx.core/tail-batch? (= (last decompressed) :done)
+   :onyx.core/requeue? false
+   :onyx.core/decompressed (remove (partial = :done) decompressed)})
+
+(defmethod p-ext/apply-fn [:input :dynamodb-scan]
+  [{:keys [onyx.core/decompressed]}]
+  {:onyx.core/results decompressed})
 
 (defmethod p-ext/apply-fn [:output :dynamodb]
   [_] {})
